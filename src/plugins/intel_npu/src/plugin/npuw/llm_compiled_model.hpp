e1af2f823807d9b58c2905b74c7ec82360df3410
Ekaterina Shiryaeva <ekaterina.shiriaeva@intel.com>
2025-08-07 12:40:00 +0100

    Moved whisper pipeline transformations to NPUW

--- src/plugins/intel_npu/src/plugin/npuw/llm_compiled_model.hpp
+++ src/plugins/intel_npu/src/plugin/npuw/llm_compiled_model.hpp
@@ -12,6 +12,7 @@ namespace ov {
 namespace npuw {
 
 class LLMInferRequest;
+class WhisperInferRequest;
 class LLMCompiledModel : public ov::npuw::ICompiledModel {
     using GetPropertiesMap =
         std::map<std::string, std::tuple<ov::PropertyMutability, std::function<ov::Any(const ::intel_npu::Config&)>>>;
@@ -47,8 +48,10 @@ public:
 
 private:
     friend class LLMInferRequest;
+    friend class WhisperInferRequest;
 
     std::shared_ptr<ov::ISyncInferRequest> create_llm_infer_request();
+    std::shared_ptr<ov::ISyncInferRequest> create_whisper_infer_request();
     std::shared_ptr<ov::ISyncInferRequest> create_sync_infer_request() const override;
     void implement_properties();
 
@@ -74,6 +77,8 @@ private:
     std::shared_ptr<ov::npuw::CompiledModel> m_prefill_compiled;
     // This model is optional, so can be null.
     std::shared_ptr<ov::npuw::CompiledModel> m_lm_head_compiled;
+
+    bool m_is_whisper = false;
 };
 
 }  // namespace npuw
